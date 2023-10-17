f13da7e8b77f0333544f36de80bc8e5842d31653
Nikolai <nikolai.zakharov@edu.salpaus.fi>
2023-10-17 14:15:32 +0300

    etusivu

---
+++ js/etusivu.js
@@ -0,0 +1,64 @@
+$('document').ready(function(){
+    let currentImage = 0;
+    $('.edellinen').click((e) => {
+        e.preventDefault();
+        previousImage();
+    });
+    $('.seuraava').click((e) => {
+        e.preventDefault();
+        nextImage();
+    });
+    let images = [
+        {
+            otsikko: "test 1",
+            teksti: "testi teksti 1",
+            kuva: "img/talonerikoinen.jpg"
+        },
+        {
+            otsikko: "test 2",
+            teksti: "testi teksti 2",
+            kuva: "img/kanakebab.jpg"
+        }
+    ];
+    function drawImage(index){
+        $('.uutuudet-img').css("background-image","url("+images[index].kuva+")");
+        $('.uutuudet-teksti h3').html(images[index].otsikko);
+        $('.uutuudet-teksti p').html(images[index].teksti);
+    }
+    drawImage(0);
+
+    const interval = setInterval(function() {
+        nextImage
+    }, 5000);
+
+    function nextImage(){
+        if(currentImage === images.length-1){
+            currentImage = 0;
+        }else{
+            currentImage += 1;
+        }
+        drawImage(currentImage);
+    }
+
+    function previousImage(){
+        if(currentImage === 0){
+            currentImage = images.length-1;
+        }else{
+            currentImage -= 1;
+        }
+        drawImage(currentImage);
+    }
+});
+/*let x = 3;
+function button1() {
+    x += 1;
+}
+function button2() {
+    x -= 1;
+}
+if (x > 2) {
+    y = "Talon Erikoinen";
+} else if (x > 1) {
+    y = "Talosta Erikoinen";
+}
+document.write(y);*/
